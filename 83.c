35314db07084ea37a44764a9eaf1e2bd87f64183
manisu <35994839+manisu@users.noreply.github.com>
2018-03-23 15:52:27 +0530

    Create 83.c

---
+++ 83.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{
+	int a,b,c,i;
+	char op;
+  clrscr();
+	printf("\n enter the number and operator(/ or % ) and a number :\n");
+	for(i=1;i<=4;i++)
+	{
+		scanf("%d %c %d",&a,&op,&b);
+	}
+		if(op=='/')
+		{
+			c=a/b;
+			printf("%d\n",c);
+		}
+		else
+		{
+			c=a%b;
+			printf("%d\n",c);
+		}
+getch();
+}
