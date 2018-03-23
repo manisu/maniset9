9a86a80086c23e5475574ca404399f88d0f19b6b
manisu <35994839+manisu@users.noreply.github.com>
2018-03-23 15:58:52 +0530

    Create 85.c

---
+++ 85.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char a[20];
+	int i,b;
+	printf("enter the string:");
+	scanf("%s",&a);
+	b=strlen(a);
+	for(i=0;i<b;i=i+2)
+	{
+		printf(" %c",a[i]);
+	}
+	for(i=1;i<b;i=i+2)
+	{
+		printf("%c",a[i]);
+	}
+	getch();
+}
